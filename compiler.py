501b0a13bd67caab7e5f2e148e316f4456c5023c
Gregory Koberger <gkoberger@mozilla.com>
2010-09-28 14:45:24 -0700

    Adding files

---
+++ compiler.py
@@ -0,0 +1,63 @@
+import os, os.path, shutil
+
+YUI_COMPRESSOR = 'yuicompressor-2.4.2.jar'
+
+def compress(in_files, out_file, in_type='js', verbose=False,
+             temp_file='.temp'):
+    temp = open(temp_file, 'w')
+    for f in in_files:
+        fh = open(f)
+        data = fh.read() + '\n'
+        fh.close()
+
+        temp.write(data)
+
+        print ' + %s' % f
+    temp.close()
+
+    options = ['-o "%s"' % out_file,
+               '--type %s' % in_type]
+
+    if verbose:
+        options.append('-v')
+
+    os.system('java -jar "%s" %s "%s"' % (YUI_COMPRESSOR,
+                                          ' '.join(options),
+                                          temp_file))
+
+    org_size = os.path.getsize(temp_file)
+    new_size = os.path.getsize(out_file)
+
+    print '=> %s' % out_file
+    print 'Original: %.2f kB' % (org_size / 1024.0)
+    print 'Compressed: %.2f kB' % (new_size / 1024.0)
+    print 'Reduction: %.1f%%' % (float(org_size - new_size) / org_size * 100)
+    print ''
+
+    #os.remove(temp_file)
+
+
+SCRIPTS = [
+        'js/jquery.js',
+        'js/md5.js',
+        'js/date.js',
+        'js/bugzilla.js',
+        ]
+SCRIPTS_OUT_DEBUG = 'min/bugzilla.debug.js'
+SCRIPTS_OUT = 'min/bugzilla.min.js'
+
+STYLESHEETS = [
+    'style.css',
+    ]
+STYLESHEETS_OUT = 'min/style.min.css'
+
+def main():
+    print 'Compressing JavaScript...'
+    compress(SCRIPTS, SCRIPTS_OUT, 'js', False, SCRIPTS_OUT_DEBUG)
+
+    print 'Compressing CSS...'
+    compress(STYLESHEETS, STYLESHEETS_OUT, 'css')
+
+if __name__ == '__main__':
+    main()
+
---
+++ js/bugzilla.js
@@ -0,0 +1,265 @@
+var settings = [];
+var settings_fields = [];
+var bug_id = false;
+var joinCount = 0;
+
+$(document).ready(function(){
+    registerPref('changes', 'Show changes to the bug');
+    registerPref('git', 'Show git log inline');
+    registerPref('gallery', 'Display images and attachments as an inline gallery');
+    registerPref('prettydate', 'Turn timestamps into a nice looking date');
+    registerPref('gravatar', 'Show gravatars for comments');
+    registerPref('gitcomments', 'Use git-style comments?');
+
+    /** Get the bug ID **/
+
+    bug_id = $('title').text().match(/Bug ([0-9]+)/)
+    bug_id = bug_id ? bug_id[1] : false
+
+    /** Run the modules **/
+
+    addPrefs();
+
+    if(bug_id) {
+
+        if (settings['gitcomments']) {
+            $('body').addClass('git_style')
+        }
+
+        if (settings['changes']) {
+            loadChanges();
+        }
+
+        if(settings['gravatar']) {
+            loadGravatars();
+        }
+
+        if(settings['prettydate']) {
+            loadPrettydate();
+        }
+
+        if (settings['gallery'] || settings['git']) {
+            parseLinks();
+        }
+    }
+});
+
+function addPrefs() {
+    $("<span class='separator'>| </span><li><a href=''>BugzillaJS Preferences</a></li>").appendTo('#header .links, #links-actions .links').click(function(){
+        $('#prefs').remove();
+
+        prefs = $('<div id="prefs">').appendTo('body')
+        $.each(settings_fields, function(k, v){
+            o = "<div><input type='checkbox' id='setting_"+v.slug+"' data-slug='"+v.slug+"' "+(settings[v.slug] ? "checked='checked'" : "")+">";
+            o += "<label for='setting_"+v.slug+"'>" + v.details + "</label></div>";
+            prefs.append(o);
+        });
+
+        $("<a href='#'>close preferences</a>").appendTo(prefs).click(function(){ $('#prefs').remove(); return false; });
+        $("<span>&nbsp;|&nbsp;</span><a href='#'>refresh page</a>").appendTo(prefs).click(function(){ window.location.reload(); return false; });
+
+        $('input', prefs).change(function(){
+            window.localStorage['settings_' + $(this).attr('data-slug')] =
+            settings[$(this).attr('data-slug')] =
+            $(this).is(':checked') ? 1 : 0;
+        });
+
+        return false;
+    });
+
+}
+
+function registerPref(slug, details) {
+    settings[slug] = window.localStorage['settings_' + slug] == null ? true : window.localStorage['settings_' + slug]*1;
+    settings_fields.push({'slug':slug, 'details':details});
+}
+
+function loadPrettydate() {
+    $('.bz_comment_time').each(function () {
+        $(this).attr('title', $(this).text().trim()).html(prettydate(Date.parse($(this).text().replace(/PDT/, 'PST'))))
+    });
+}
+
+function loadGravatars() {
+    $('.bz_comment_head,.bz_first_comment_head').each(function () {
+        email = $('a[class=email]', this).attr('href').replace(/mailto:\s*/, '')
+        $(this).prepend('<img src="http://www.gravatar.com/avatar/' + hex_md5(email) + '">');
+    });
+}
+
+function loadChanges() {
+
+    // Load comments
+    // (We need to do it this way because the DOM doesn't give us enough info)
+
+    comments = [];
+
+    $.getJSON('https://api-dev.bugzilla.mozilla.org/latest/bug/' + bug_id + '/comment', function (d) {
+
+        //-- Add comments to the comment array
+
+        $.each(d.comments, function (v, k) {
+            comments.push({
+                'date': new Date(k.creation_time).getTime(),
+                'type': 'comment'
+            });
+        });
+
+        //-- Associate comments from the API with comments in the DOM
+
+        var i = 0;
+        $('.bz_comment').each(function () {
+            $(this).attr('id', 'd' + new Date(d.comments[i].creation_time).getTime());
+            i++;
+        });
+
+        joinComments();
+
+    });
+
+    // Now, load the changes from the API
+
+    changes = [];
+
+    $.getJSON('https://api-dev.bugzilla.mozilla.org/latest/bug/' + bug_id + '/history', function (d) {
+        $.each(d.history, function (v, k) {
+            changes.push({
+                'date': new Date(k.change_time).getTime(),
+                'change': k,
+                'type': 'change'
+            })
+        });
+        joinComments();
+
+    });
+
+}
+
+// Join the comments after both changes and comments have loaded
+
+function joinComments() {
+    joinCount++;
+    if (joinCount < 2) return;
+
+    // Combine and sort the comments / changes
+
+    everything = $.merge(comments, changes).sort(function (x, y) {
+        return (x.date + (x.type == 'change')) - (y.date + (y.type == 'change'));
+    });
+
+    // Now, loop through them and add to the DOM
+
+    comment = everything[0];
+
+    $.each(everything, function (k, v) {
+        if (v.type == 'comment') {
+            comment = v
+        } else if (v.date == comment.date) {
+            $('#d' + comment.date).find('.bz_comment_text').prepend('<div class="history">' + formatChange(v.change.changes) + '</div>');
+        } else {
+            $('#d' + comment.date).after('<div class="history"><strong>' + v.change.changer.name + '</strong> ' + formatChange(v.change.changes) + '</div>');
+        }
+    });
+
+}
+
+// Return a formatted version of the changes
+
+function formatChange(c) {
+    changes_array = [];
+    $.each(c, function (ck, cv) {
+        changes_array.push(cv.field_name + ": " + (cv.removed ? "<del>" + cv.removed + "</del> => " : "") + cv.added)
+    });
+
+    return changes_array.join('; ');
+}
+
+function parseLinks() {
+    $('.bz_comment_text').each(function () {
+        comment = $(this);
+
+        (function (comment) {
+            var images = new Array();
+            $('a', comment).each(function () {
+                if (settings['gallery'] && $(this).attr('href').match(/(\.(jpg|gif|png))/i)) {
+                    images.push($(this).attr('href'));
+                } else if (settings['git'] && (t = $(this).attr('href').match(/github.com\/([a-zA-Z0-9]*)\/([a-zA-Z0-9]*)\/commit\/([a-zA-Z0-9]*)/))) {
+
+                    jQuery.getJSON('http://github.com/api/v2/json/commits/show/' + t[1] + '/' + t[2] + '/' + t[3] + '?callback=?', function (d) {
+
+                        git = $('<div class="git-body">').insertAfter(comment);
+                        head = $('<div class="git-head">').appendTo(git);
+
+                        head.append('<p>' + d.commit.message + '</p>');
+                        head.append("<span><strong>" + d.commit.author.name + '</strong>&nbsp;' + prettydate(new Date(d.commit.authored_date)) + '</span>');
+
+                        table = $("<table class='git'></table>").appendTo(git);
+
+                        if (d.commit.added) {
+                            $.each(d.commit.added, function (k, v) {
+                                table.append("<tr><td class='add'><div class='stat-icon'></td><td>" + v + "</td></tr><td>" + gitBox(5, 0) + "</td>");
+                            });
+                        }
+
+                        if (d.commit.modified) {
+                            $.each(d.commit.modified, function (k, v) {
+                                additions = (v.diff.match(/^\+[^+]/gm) || '').length
+                                deletions = (v.diff.match(/^\-[^-]/gm) || '').length
+                                table.append("<tr><td class='modify'><div class='stat-icon'></div></td><td>" + v.filename + "</td><td>" + gitBox(additions, deletions) + "</td></tr>");
+
+                            });
+                        }
+                    });
+                }
+            });
+
+            if (settings['gallery'] && images.length) {
+                gal = $('<div class="img-gal">').insertAfter(comment);
+                $.each(images, function (k, v) {
+                    $(gal).append('<a href="' + v + '" title="' + v + '" target="_blank"><img src="' + v + '"></a>')
+                });
+            }
+
+        })(comment);
+    });
+}
+
+function gitBox(a, d) {
+    total = a + d;
+    if (total > 5) {
+        a = Math.round((a / total) * 5)
+        d = Math.round((d / total) * 5)
+    }
+
+    output = "";
+    t = 0;
+
+    for (i = 0; i < a && t < 5; i++, t++) output += "<span class='ga'/>";
+    for (i = 0; i < d && t < 5; i++, t++) output += "<span class='gd'/>";
+    for (i = 0; i < 5 && t < 5; i++, t++) output += "<span class='gb'/>";
+
+    return output;
+}
+
+
+function set_cookie( name, value ) {
+  var cookie_string = name + "=" + escape ( value );
+
+  if ( exp_y )
+  {
+    var expires = new Date (  );
+    cookie_string += "; expires=" + expires.toGMTString();
+  }
+        cookie_string += "; secure";
+
+  document.cookie = cookie_string;
+}
+function get_cookie ( cookie_name )
+{
+  var results = document.cookie.match ( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
+
+  if ( results )
+    return ( unescape ( results[2] ) );
+  else
+    return null;
+}
