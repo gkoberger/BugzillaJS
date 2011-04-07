a74cbd60d5e5320c6ac3fb01d581b29b3dee41c1
Gregory Koberger <gkoberger@gmail.com>
2011-04-06 23:42:55 -0700

    Use secure gravatars

--- includes/bugzilla-gravatars.js
+++ includes/bugzilla-gravatars.js
@@ -6,7 +6,7 @@ function setGravatars() {
             // Email addresses are only shown if the user is logged in
             if($('a.email', this).length > 0) {
                 email = $('a.email', this).attr('href').replace(/mailto:\s*/, '');
-                $(this).prepend('<img src="http://www.gravatar.com/avatar/' + hex_md5(email) + '">');
+                $(this).prepend('<img src="https://secure.gravatar.com/avatar/' + hex_md5(email) + '">');
             }
 
         });
