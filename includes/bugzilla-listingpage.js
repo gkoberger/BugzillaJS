5fd93bba05c4d04c619da6c8e8534eccfc228d07
Gregory Koberger <gkoberger@gmail.com>
2011-11-30 18:13:48 -0800

    Fix a small bug with opening all in tabs

--- includes/bugzilla-listingpage.js
+++ includes/bugzilla-listingpage.js
@@ -5,14 +5,12 @@ function loadOpenAll() {
         e.preventDefault();
 
         var all = $('.bz_id_column a');
-        if(all.length > 10) {
-            if(confirm("You're trying to open " + all.length + " tabs. Are you sure you want to do this?")) {
-                $('.bz_id_column a').each(function() {
-                    if($(this).attr('href')) {
-                        window.open($(this).attr('href'),'_blank');
-                    }
-                });
-            }
+        if(all.length < 10 || confirm("You're trying to open " + all.length + " tabs. Are you sure you want to do this?")) {
+            $('.bz_id_column a').each(function() {
+                if($(this).attr('href')) {
+                    window.open($(this).attr('href'),'_blank');
+                }
+            });
         }
     }
 
