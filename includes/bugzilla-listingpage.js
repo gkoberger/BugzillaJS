319ac18524ed56ad112aebe9ce18444817115e6a
Gregory Koberger <gkoberger@gmail.com>
2011-11-30 18:05:51 -0800

    Add "Open all in tabs" link

---
+++ includes/bugzilla-listingpage.js
@@ -0,0 +1,25 @@
+registerPref('openall', 'Option to open all bugs in tabs?', loadOpenAll);
+
+function loadOpenAll() {
+    var openAll = function(e) {
+        e.preventDefault();
+
+        var all = $('.bz_id_column a');
+        if(all.length > 10) {
+            if(confirm("You're trying to open " + all.length + " tabs. Are you sure you want to do this?")) {
+                $('.bz_id_column a').each(function() {
+                    if($(this).attr('href')) {
+                        window.open($(this).attr('href'),'_blank');
+                    }
+                });
+            }
+        }
+    }
+
+    var a = $('<a>', {'href': '#', 'click': openAll, 'text': 'Open All in Tabs'}),
+        pipe = $('<span>', {'html': '&nbsp;|'}),
+        td = $('<td>');
+
+    td.append(a).append(pipe);
+    $('.bz_query_edit').before(td);
+}
