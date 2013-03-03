fb89215ca41c979dcc6fc440c97212716db31f28
Anthony Ricaud <anthony@ricaud.me>
2013-03-03 21:51:40 +0100

    TBPL results iframe resizes when focused
    - Default height is 200px
    - Resizes to 600px when focused
    - Adds a border to make it clear it's a different part of the page
    - Fixes order of inserted TBPL results to match the order of links
    
    fixes #66

--- includes/bugzilla-tbpl.js
+++ includes/bugzilla-tbpl.js
@@ -7,10 +7,34 @@ function initTBPL() {
     if (!settings['show_tbpl']) {
         return;
     }
-    bz_comments.find('a[href^="https://tbpl.mozilla.org/?"]').each(function() {
-        var $comment = $(this).parent();
-        var src = $(this).attr('href') + '&embed=true';
-        var $iframe = $('<iframe src="' + src + '" class="tbpl_inline_result">');
-        $comment.after($iframe);
-    });
+
+    document.addEventListener('focus', detectIframeFocused);
+    document.addEventListener('blur', detectIframeFocused);
+
+    var tbpls = document.querySelectorAll('.bz_comment_text a[href^="https://tbpl.mozilla.org/?"]');
+    for (var i=0, il=tbpls.length; i < il; i++) {
+        var tbpl = tbpls[i];
+
+        var iframe = document.createElement('iframe');
+        iframe.src = tbpl.href + '&embed=true';
+        iframe.classList.add('tbpl_inline_result');
+
+        tbpl.parentNode.appendChild(iframe);
+    }
+}
+
+// TODO : Once dropping Ominum https://github.com/gkoberger/BugzillaJS/issues/69,
+// do this more properly by including a script in the iframes
+function detectIframeFocused() {
+    var iframes = document.getElementsByClassName('iframe_focused');
+    if (iframes.length) {
+        iframes[0].classList.remove('iframe_focused');
+    }
+
+    // We need a timeout here, otherwise document.activeElement has not changed yet
+    setTimeout(function () {
+        if (document.activeElement.nodeName.toLowerCase() == 'iframe') {
+            document.activeElement.classList.add('iframe_focused');
+        }
+    }, 0);
 }
