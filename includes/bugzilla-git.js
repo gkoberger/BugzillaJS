8be3125bd599802a9268678cad6600d7df9871c0
Anthony Ricaud <anthony@ricaud.me>
2015-01-01 19:39:21 +0100

    'Show GitHub logs inline' is broken
    - Switch to GitHub API v3
    - Remove JSONP since it is not needed anymore
    
    fix #106

--- includes/bugzilla-git.js
+++ includes/bugzilla-git.js
@@ -1,83 +1,96 @@
+'use strict';
+
+/* global registerPref, ifBug, bz_comments, $, repositionScroll */
+
 registerPref('git', {'title': 'Show GitHub logs inline',
                      'setting_default': true,
                      'callback': ifBug(initGit),
                      'category': 'inline'});
 
 function initGit() {
+    var github_regexp = /github.com\/([-\w]*)\/([-\w]*)\/commit\/(\w*)/;
     bz_comments.each(function() {
-        var comment = $(this);
-        $('a', comment).each(function () {
-            var url = $(this).attr('href')
-                isGithub = url.match(/github.com\/([-\w]*)\/([-\w]*)\/commit\/(\w*)/);
+        var $comment = $(this);
+        $('a', $comment).each(function() {
+            var url = $(this).attr('href');
+            var isGithub = url.match(github_regexp);
+            console.log('fooo', isGithub);
 
-            if (isGithub) {
-                giturl = 'https://github.com/api/v2/json/commits/show/' +
-                         isGithub[1] + '/' + isGithub[2] + '/' + isGithub[3];
-                         //'?callback=cbk'
-
-
-
-                JSONP.get( giturl, {}, function (d) {
-                    git = $('<div>', {'class': "git-body"}).insertAfter(comment);
-                    head = $('<div>', {'class': "git-head"}).appendTo(git);
-
-                    head.append($('<p>', {'text': d.commit.message}));
-
-                    var $span = $('<span>'),
-                        $strong = $('<strong>', {'text': d.commit.author.name}),
-                        $date = $('<span>', {'text': d.commit.authored_date});
-
-                    $span.append($strong).append($date);
-                    head.append($span);
-
-                    table = $("<table>", {'class': 'git'}).appendTo(git);
-
-                    if (d.commit.added) {
-                        $.each(d.commit.added, function (k, v) {
-                            table.append("<tr><td class='add'>" +
-                                "<div class='stat-icon'></td><td>" + v +
-                                "</td><td class='changes_td'>" + gitBox(5, 0) + "</td></tr>");
-                        });
-                    }
-
-                    if (d.commit.removed) {
-                        $.each(d.commit.removed, function (k, v) {
-                            table.append("<tr><td class='remove'><div class='stat-icon'></td><td>" + v +
-                                "</td><td class='changes_td'>" + gitBox(0, 5) + "</td></tr>");
-                        });
-                    }
-
-                    if (d.commit.modified) {
-                        $.each(d.commit.modified, function (k, v) {
-                            additions = (v.diff.match(/^\+[^+]/gm) || '').length
-                            deletions = (v.diff.match(/^\-[^-]/gm) || '').length
-                            table.append("<tr><td class='modify'><div class='stat-icon'></div></td><td>" +
-                                v.filename + "</td><td class='changes_td'>" +
-                                gitBox(additions, deletions) + "</td></tr>");
-
-                        });
-                    }
-                    repositionScroll();
-                });
+            if (!isGithub) {
+                return;
             }
+
+            var giturl = 'https://api.github.com/repos/' + isGithub[1] + '/' +
+                     isGithub[2] + '/commits/' + isGithub[3];
+                     //'?callback=cbk'
+
+            $.getJSON(giturl, function(d) {
+                var $git = $('<div>', {'class': 'git-body'}).
+                    insertAfter($comment);
+                var $head = $('<div>', {'class': 'git-head'}).appendTo($git);
+
+                $head.append($('<p>', {'text': d.commit.message}));
+
+                var $span = $('<span>'),
+                    $strong = $('<strong>', {'text': d.commit.author.name}),
+                    $date = $('<span>', {'text': d.commit.author.date});
+
+                $span.append($strong).append($date);
+                $head.append($span);
+
+                var $table = $('<table>', {'class': 'git'}).appendTo($git);
+
+                $.each(d.files, function(k, file) {
+                    if (file.status === 'added') {
+                        $table.append('<tr><td class="add">' +
+                            '<div class="stat-icon"></td><td>' + file.filename +
+                            '</td><td class="changes_td">' + gitBox(5, 0) +
+                            '</td></tr>');
+                    }
+
+                    if (file.status === 'removed') {
+                        $table.append('<tr><td class="remove">' +
+                            '<div class="stat-icon"></td><td>' + file.filename +
+                            '</td><td class="changes_td">' + gitBox(0, 5) +
+                            '</td></tr>');
+                    }
+
+                    if (file.status === 'modified') {
+                        $table.append('<tr><td class="modify">' +
+                            '<div class="stat-icon"></div></td><td>' +
+                            file.filename + '</td><td class="changes_td">' +
+                            gitBox(file.additions, file.deletions) +
+                            '</td></tr>');
+                    }
+                });
+                repositionScroll();
+            });
         });
     });
 
-    function gitBox(a, d) {
-        output = "<div>" + a + " additions & " + d + " deletions</div>";
+    function gitBox(added, deleted) {
+        var output = '<div>' + added + ' additions & ' +
+            deleted + ' deletions</div>';
 
-        total = a + d;
+        var total = added + deleted;
         if (total > 5) {
-            a = Math.round((a / total) * 5)
-            d = Math.round((d / total) * 5)
+            added = Math.round((added / total) * 5);
+            deleted = Math.round((deleted / total) * 5);
         }
 
-        t = 0;
+        var t = 0;
+        var i;
+        for (i = 0; i < added && t < 5; i++, t++) {
+            output += '<span class="ga"/>';
+        }
+        for (i = 0; i < deleted && t < 5; i++, t++) {
+            output += '<span class="gd"/>';
+        }
+        for (i = 0; i < 5 && t < 5; i++, t++) {
+            output += '<span class="gb"/>';
+        }
 
-        for (i = 0; i < a && t < 5; i++, t++) output += "<span class='ga'/>";
-        for (i = 0; i < d && t < 5; i++, t++) output += "<span class='gd'/>";
-        for (i = 0; i < 5 && t < 5; i++, t++) output += "<span class='gb'/>";
-
-        return "<div class='changes_box'><strong>" + total + "</strong> " + output + "</div>";
+        return '<div class="changes_box"><strong>' + total + '</strong> ' +
+            output + '</div>';
     }
 }
