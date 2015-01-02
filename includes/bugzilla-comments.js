109d363d4baa76edf7acbc305cb70e5b4fab1664
Anthony Ricaud <anthony@ricaud.me>
2015-01-02 20:09:00 +0100

    Restore 'Highlight reporter and assignee comments' feature
    - Fix call to registerPref
    - Fixed the DOM selections
    - Rewritten without jQuery dependency (except bz_comments)
    
    fix #105

--- includes/bugzilla-comments.js
+++ includes/bugzilla-comments.js
@@ -1,38 +1,45 @@
 'use strict';
 
-/*global registerPref, ifBug, settings, $, bz_comments */
+/*global registerPref, ifBug, settings, bz_comments */
 
-registerPref('reporter_assignee', 'Highlight reporter and assignee comments?',
-    ifBug(initHighlightRA));
+registerPref('reporter_assignee',
+    {title: 'Highlight reporter and assignee comments?',
+    callback: ifBug(initHighlightRA),
+    category: 'comments'});
 
 function initHighlightRA() {
-    if (settings.reporter_assignee) {
-        var assignee = false,
-            reporter = false;
-
-        // Figure out who's the assignee and who's the reporter
-        $('.vcard').each(function(i, el) {
-            var t = $(el).closest('tr').find('b').text();
-            if (t == 'Assigned To') {
-                assignee = $(el).text();
-            } else if (t == 'Reported') {
-                reporter = $(el).text();
-            }
-        });
-
-        bz_comments.each(function() {
-            var $comment = $(this).parent(),
-                text = $comment.find('.vcard').text();
-            if (text == assignee) {
-                $comment.find('.fn').after($('<span>',
-                    {'text': '(assignee)', 'class': 'assignee'}));
-                $comment.addClass('bz_assignee');
-            }
-            if (text == reporter) {
-                $comment.find('.fn').after($('<span>',
-                    {'text': '(reporter)', 'class': 'reporter'}));
-                $comment.addClass('bz_reporter');
-            }
-        });
+    if (!settings.reporter_assignee) {
+        return;
     }
+
+    var selector = '#bz_assignee_edit_container .vcard .fn';
+    var assignee = document.querySelector(selector).textContent;
+
+    var reporter = false;
+    for (var field of document.querySelectorAll('.field_label')) {
+        if (field.textContent.contains('Reported')) {
+            reporter = field.nextElementSibling.querySelector('.vcard .fn').
+                textContent;
+            break;
+        }
+    }
+
+    bz_comments.each(function(i, comment) {
+        var commenter = comment.parentNode.querySelector('.vcard .fn');
+        var span;
+
+        if (commenter.textContent == assignee) {
+            span = document.createElement('span');
+            span.textContent = '(assignee)';
+            span.classList.add('assignee');
+            commenter.parentNode.insertBefore(span, commenter.nextSibling);
+        }
+
+        if (commenter.textContent == reporter) {
+            span = document.createElement('span');
+            span.textContent = '(reporter)';
+            span.classList.add('reporter');
+            commenter.parentNode.insertBefore(span, commenter.nextSibling);
+        }
+    });
 }
